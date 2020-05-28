45980f9bee6cc350fee96319ee470b2bc1ab7465
pgomez-cambronero-earthrover <pablo.gomez-cambronero@earthrover.farm>
2020-05-28 11:50:47 +0200

    hotfix param no channel

--- roboteq_motor_controller_driver/src/config_client.cpp
+++ roboteq_motor_controller_driver/src/config_client.cpp
@@ -1,15 +1,25 @@
 #include "ros/ros.h"
 #include <roboteq_motor_controller_driver/config_srv.h>
- 
+
 int main(int argc, char **argv)
  {
    ros::init(argc, argv, "config_client");
    ros::NodeHandle nh;
    ros::ServiceClient client = nh.serviceClient<roboteq_motor_controller_driver::config_srv>("config_service");
    roboteq_motor_controller_driver::config_srv srv;
-   srv.request.userInput = argv[1];
-   srv.request.channel = atoll(argv[2]);
-   srv.request.value = atoll(argv[3]);
+   if(argc==3)
+   {
+     srv.request.userInput = argv[1];
+     srv.request.channel = 0;
+     srv.request.value = atoll(argv[2]);
+   }
+   else
+   {
+     srv.request.userInput = argv[1];
+     srv.request.channel = atoll(argv[2]);
+     srv.request.value = atoll(argv[3]);
+   }
+
    if (client.call(srv))
    {
      ROS_INFO("success!");
@@ -19,6 +29,6 @@ int main(int argc, char **argv)
      ROS_ERROR("Failed to call service");
      return 1;
    }
-  
+
    return 0;
  }
