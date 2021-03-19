89b4525aefb24da18961a359e6ed8d2fb79696f1
Xavier Ruiz <xavier.ruiz@earthrover.farm>
2021-03-19 19:32:32 +0100

    [works] load entire profile at once & load manual profile upon start

--- roboteq_motor_controller_driver/src/main.cpp
+++ roboteq_motor_controller_driver/src/main.cpp
@@ -7,10 +7,17 @@
 
 int main(int argc, char** argv){
 	ros::init(argc, argv, "roboteq_motor_controller_driver");
-	
-	roboteq::Driver driver;	
+
+	roboteq::Driver driver;
 	driver.connect();
 	driver.roboteq_services();
+
+	ROS_INFO_STREAM("LOADING MANUAL PROFILE");
+	bool loaded = driver.load_manual_profile();
+	if (loaded)
+		ROS_INFO("LOADED PROFILE!");
+	else
+		ROS_WARN("COULD NOT LOAD MANUAL PROFILE");
 
 	driver.run();
 
@@ -23,7 +30,7 @@ int main(int argc, char** argv){
 
 	ros::waitForShutdown();
 	//spinner.stop();
-		
+
 	return 0;
 
 
